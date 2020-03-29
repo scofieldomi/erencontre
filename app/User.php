1356b9e8c4b50a2ddcb19643160e7d6dbc4ce097
bagre <bagre.dominique@gmail.com>
2020-03-29 00:13:51 +0000

    mon premier push

--- app/User.php
+++ app/User.php
@@ -15,8 +15,9 @@ class User extends Authenticatable
      *
      * @var array
      */
+    
     protected $fillable = [
-        'name', 'email', 'password',
+        'name', 'email', 'password'
     ];
 
     /**
@@ -24,6 +25,7 @@ class User extends Authenticatable
      *
      * @var array
      */
+
     protected $hidden = [
         'password', 'remember_token',
     ];
@@ -36,4 +38,5 @@ class User extends Authenticatable
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+
 }
